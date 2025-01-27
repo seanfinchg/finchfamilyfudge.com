c97953c4a5906a7b3ff9badc7f4fc32d3173d7be
Sean Finch <145179592+seanfinchg@users.noreply.github.com>
2025-01-27 00:48:47 -0500

    Update App.tsx
    Manually define routes

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,12 @@ const App: React.FC = () => {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/products" element={<Products />} />
-              <Route path="/products/:slug" element={<ProductPage />} />{" "}
+              {/* Manually defined routes for each product */}
+              <Route path="/products/almond-roca-chocolate-fudge" element={<ProductPage />} />
+              <Route path="/products/rum-cherry-chocolate-fudge" element={<ProductPage />} />
+              <Route path="/products/butterscotch-pecan-fudge" element={<ProductPage />} />
+              <Route path="/products/peppermint-fudge" element={<ProductPage />} />
+              <Route path="/products/almond-roca" element={<ProductPage />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/checkout" element={<Checkout />} />
               <Route path="/about" element={<About />} />{" "}
